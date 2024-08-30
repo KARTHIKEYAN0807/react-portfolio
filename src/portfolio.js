4171fe084ed4eee9dcbc4325ef526f4c7a7e519c
KARTHIKEYAN0807 <karthickestore.37sp@gmail.com>
2024-08-30 14:51:57 +0530

    portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -292,83 +292,83 @@ const certifications = {
       color_code: "#0C9D5899",
     },
     {
-      title: "Data Science",
-      subtitle: "- Alex Aklson",
-      logo_path: "ibm_logo.png",
+      title: "JAVA",
+      subtitle: "- GUVI",
+      logo_path: "java2.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/PLEAPCSJBZT5",
-      alt_name: "IBM",
+        "https://www.guvi.in/certificate?id=61R08ub50823179TOj",
+      alt_name: "guvi",
       color_code: "#1F70C199",
     },
     {
-      title: "Big Data",
-      subtitle: "- Kim Akers",
-      logo_path: "microsoft_logo.png",
+      title: "Enterprise Design Thinking Co-Creator",
+      subtitle: "- IBM",
+      logo_path: "ibm.png",
       certificate_link:
-        "https://drive.google.com/file/d/164zKCFOsI4vGqokc-Qj-e_D00kLDHIrG/view",
-      alt_name: "Microsoft",
+        "https://www.credly.com/badges/321b9b8f-0ef3-4762-a8ec-2ee8a43c0fef/print",
+      alt_name: "IBM",
       color_code: "#D83B0199",
     },
     {
-      title: "Advanced Data Science",
-      subtitle: "- Romeo Kienzler",
-      logo_path: "ibm_logo.png",
+      title: "AWS CustomerCast - AWS Copilot - Simplifying Container Development",
+      subtitle: "- AWS",
+      logo_path: "aws.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/BH2T9BRU87BH",
+        "https://drive.google.com/file/d/1fbjbUcC_Htn1-S7dD0Tev-8kFaCp2vfU/view?usp=sharing",
       alt_name: "IBM",
       color_code: "#1F70C199",
     },
     {
-      title: "Advanced ML on GCP",
-      subtitle: "- GCP Training",
-      logo_path: "google_logo.png",
+      title: "WEB DEVELOPMENT",
+      subtitle: "- Internship",
+      logo_path: "intern.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/5JZZM7TNQ2AV",
-      alt_name: "Google",
+        "https://drive.google.com/file/d/1zEa6fU0_2iMLOYyFfrlqtluEkrEyptnM/view?usp=sharing",
+      alt_name: "intern",
       color_code: "#0C9D5899",
     },
     {
-      title: "DL on Tensorflow",
-      subtitle: "- Laurence Moroney",
-      logo_path: "deeplearning_ai_logo.png",
+      title: "WEB DEVELOPMENT",
+      subtitle: "- Internship",
+      logo_path: "intern2.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/6T4DCUGNK8J8",
-      alt_name: "deeplearning.ai",
+        "https://drive.google.com/file/d/1kgTGnRB6_Km6K_9vvBumI4g50NzeAYr5/view?usp=sharing",
+      alt_name: "intern",
       color_code: "#00000099",
     },
     {
-      title: "Fullstack Development",
-      subtitle: "- Jogesh Muppala",
-      logo_path: "coursera_logo.png",
+      title: "Oracle Certified Foundation Associate",
+      subtitle: "- ORACLE",
+      logo_path: "oracle.png",
       certificate_link:
-        "https://www.coursera.org/account/accomplishments/certificate/NRANJA66Y2YA",
-      alt_name: "Coursera",
+        "https://drive.google.com/file/d/1cnicnTRHbwKbVL9pxKALgsKl77ttHFSe/view?usp=sharing",
+      alt_name: "oracle",
       color_code: "#2A73CC",
     },
     {
-      title: "Kuberenetes on GCP",
-      subtitle: "- Qwiklabs",
+      title: "",
+      subtitle: "- ",
       logo_path: "gcp_logo.png",
       certificate_link:
-        "https://google.qwiklabs.com/public_profiles/e4d5a92b-faf6-4679-a70b-a9047c0cd750",
+        ".",
       alt_name: "GCP",
       color_code: "#4285F499",
     },
     {
-      title: "Cryptography",
-      subtitle: "- Saurabh Mukhopadhyay",
+      title: "",
+      subtitle: "- ",
       logo_path: "nptel_logo.png",
       certificate_link:
-        "https://drive.google.com/open?id=1z5ExD_QJVdU0slLkp8CBqSF3-C3g-ro_",
+        "",
       alt_name: "NPTEL",
       color_code: "#FFBB0099",
     },
     {
-      title: "Cloud Architecture",
-      subtitle: "- Qwiklabs",
+      title: "",
+      subtitle: "- ",
       logo_path: "gcp_logo.png",
       certificate_link:
-        "https://google.qwiklabs.com/public_profiles/5fab4b2d-be6f-408c-8dcb-6d3b58ecb4a2",
+        "",
       alt_name: "GCP",
       color_code: "#4285F499",
     },
@@ -380,7 +380,7 @@ const experience = {
   title: "Experience",
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+    "As an aspiring web developer, I bring a foundational understanding of web development basics, including HTML, CSS, and JavaScript. My passion for creating user-centric web experiences is matched by my eagerness to learn and grow within the field. I am excited to apply my skills in a real-world setting and contribute to meaningful projects during my internship. 🌐🚀",
   header_image_path: "experience.svg",
   sections: [
     {
@@ -388,149 +388,90 @@ const experience = {
       work: true,
       experiences: [
         {
-          title: "Machine Learning Engineer",
-          company: "TikTok Inc.",
-          company_url: "https://www.tiktok.com/en/",
-          logo_path: "tiktok_logo.png",
-          duration: "June 2023 - Present",
-          location: "San Jose, CA, USA",
+          title: "",
+          company: "",
+          company_url: "",
+          logo_path: "job.png",
+          duration: "",
+          location: "",
           description:
-            "Improving ads ranking models on the core TikTok product. Experience working on modeling two-tower architectures like DeepFM, Wide & deep learning, etc. Working on Large Language Models (LLM) pretraining and Large Multi-modal Model (LMM) finetuning strategies.",
+            " ",
           color: "#000000",
         },
-        {
-          title: "Associate AI Engineer",
-          company: "Legato Health Technology",
-          company_url: "https://legatohealthtech.com/",
-          logo_path: "legato_logo.png",
-          duration: "June 2020 - Aug 2021",
-          location: "Hyderabad, Telangana",
-          description:
-            "I am working on automating healthcare products. The projects involve automation for process improvements and for significantly enhancing the profits. I am currently working on Cancer Survival and Reoccurence Prediction. Our goal is to make AI system which scales and removes doctor dependency as much as possible.",
-          color: "#0879bf",
-        },
-        {
-          title: "Android and ML Developer",
-          company: "Muffito Incorporation",
-          company_url: "https://www.linkedin.com/company/muffito-inc/about/",
-          logo_path: "muffito_logo.png",
-          duration: "May 2018 - Oct 2018",
-          location: "Pune, Maharashtra",
-          description:
-            "I have created complete Android Application for locating Pub, Bar and beverage shops around you. I have also worked on implementation of algorithms for Face Detection, Text extraction from Image. I was involved in a team for creating complete software architecure of mobile and web application as well as admin panel for company.",
-          color: "#9b1578",
-        },
-        {
-          title: "Android Developer",
-          company: "FreeCopy Pvt. Ltd.",
-          company_url: "https://www.linkedin.com/company/freecopy/about/",
-          logo_path: "freecopy_logo.png",
-          duration: "Nov 2017 - Dec 2017",
-          location: "Ahmedabad, Gujarat",
-          description:
-            "FreeCopy is the Start up from Indian Institute of Management, Ahmedabad. I have changed the integration of the whole app from Google to Firebase. I learnt the efﬁcient ways of Data communications like Retroﬁt, Eventbus etc. I experienced the real time start up. I learnt the Design thinking of UI on perspective of People.",
-          color: "#fc1f20",
-        },
+      
+       
+       
       ],
     },
     {
       title: "Internships",
       experiences: [
         {
-          title: "Machine Learning Intern",
-          company: "TikTok Inc.",
-          company_url: "https://www.tiktok.com/en/",
-          logo_path: "tiktok_logo.png",
-          duration: "May 2022 - Aug 2022",
-          location: "San Francisco, USA",
+          title: "MERN FULL STACK DEVELOPMENT",
+          company: "Grrowup.",
+          company_url: "https://www.grrowup.com/program/full-stack-web-dev",
+          logo_path: "grrowup.png",
+          duration: "june 2024 - Aug 2024",
+          location: "Remote",
           description:
-            "Building new features on the backend recommendation system, specifically ranking algorithms for Ads that touch hundreds of millions of people around the world. Improving online and offline content ranking algorithms by performing hard sample data replays for training steps.",
+            "8-week Full Stack Web Development internship program at GrrowUP, featuring interactive live sessions. Engage deeply in practical learning, collaborate on innovative projects with experienced mentors.",
           color: "#000000",
         },
         {
-          title: "Data Science Research Intern",
-          company: "Delhivery Pvt. Ltd.",
-          company_url: "https://www.delhivery.com/",
-          logo_path: "delhivery_logo.png",
-          duration: "May 2019 - Sept 2019",
-          location: "Gurgaon, Haryana",
+          title: "Embedded system with raspberry",
+          company: "ATS Software Solution.",
+          company_url: "https://www.accenttechnosoft.com/",
+          logo_path: "ats.png",
+          duration: "jun 2023 - jul 2023",
+          location: "COIMBATORE",
           description:
-            "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+            "This program offers practical experience and mentorship, covering core concepts like microprocessors vs microcontrollers, Arduino, communication protocols, and IoT with live projects1. Internship on Raspberry Pi: This comprehensive guide to Raspberry Pi covers GPIO control, sensor interfacing, web applications, and more. It includes beginner-friendly projects and advanced techniques for interfacing sensors with Raspberry Pi2",
           color: "#ee3c26",
         },
         {
-          title: "Data Science Intern",
-          company: "Intel Indexer LLC",
+          title: "Web development",
+          company: "Techzy It Solution",
           company_url:
-            "https://opencorporates.com/companies/us_dc/EXTUID_4170286",
-          logo_path: "intel_logo.jpg",
-          duration: "Nov 2018 - Dec 2018",
-          location: "Work From Home",
+            "http://techzyit.epizy.com/?i=1",
+          logo_path: "techzy.jpeg",
+          duration: "jun 2022 - aug 2022",
+          location: "COIMBATORE",
           description:
-            "This is financial Solution Company. I have made Supervised Learning model for the company which can perform time series analysis on Stock price data for 32 companies. I have built LSTM Neural Networks Model and trained the data of 32 companies for last 2 years. This model is also used for forecasting.",
+            "My passion for web development has led me to a profound understanding of both frontend and backend technologies, culminating in the creation of a diverse portfolio of projects. Learning Path: Frontend Development: Mastered HTML, CSS, and JavaScript, building interactive and responsive user interfaces. Delved into frameworks like React and Angular to create dynamic single-page applications. Backend Development: Gained proficiency in server-side programming with Node.js and Express.js. Developed RESTful APIs and worked with databases such as MongoDB and MySQL.",
           color: "#0071C5",
         },
+        {
+          title: "Web development",
+          company: "E Soft IT Solutions.",
+          company_url: "https://www.e-soft.in/",
+          logo_path: "esoft.jpeg",
+          duration: "DEC 2021 - DEC 2021",
+          location: "Trichy 620 018",
+          description:
+            "As an aspiring web developer, I bring a foundational understanding of web development basics, including HTML, CSS, and JavaScript. My passion for creating user-centric web experiences is matched by my eagerness to learn and grow within the field. I am excited to apply my skills in a real-world setting and contribute to meaningful projects during my internship. 🌐🚀",
+          color: "#ee3c26",
+        },
+        
       ],
     },
     {
       title: "Volunteerships",
       experiences: [
         {
-          title: "Google Explore ML Facilitator",
-          company: "Google",
-          company_url: "https://about.google/",
-          logo_path: "google_logo.png",
-          duration: "June 2019 - April 2020",
-          location: "Hyderabad, Telangana",
+          title: "",
+          company: "",
+          company_url: "",
+          logo_path: "vol.jpg",
+          duration: "",
+          location: "",
           description:
-            "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
+            "",
           color: "#4285F4",
         },
-        {
-          title: "Microsoft Student Partner",
-          company: "Microsoft",
-          company_url: "https://www.microsoft.com/",
-          logo_path: "microsoft_logo.png",
-          duration: "Aug 2019 - May 2020",
-          location: "Hyderabad, Telangana",
-          description:
-            "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
-          color: "#D83B01",
-        },
-        {
-          title: "Mozilla Campus Captain",
-          company: "Mozilla",
-          company_url: "https://www.mozilla.org/",
-          logo_path: "mozilla_logo.png",
-          duration: "Oct 2019 - May 2020",
-          location: "Kurnool, Andhra Pradesh",
-          description:
-            "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
-          color: "#000000",
-        },
-        {
-          title: "Developer Students Club Member",
-          company: "DSC IIITDM Kurnool",
-          company_url:
-            "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
-          logo_path: "dsc_logo.png",
-          duration: "Jan 2018 - May 2020",
-          location: "Kurnool, Andhra Pradesh",
-          description:
-            "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
-          color: "#0C9D58",
-        },
-        {
-          title: "Developer Program Member",
-          company: "Github",
-          company_url: "https://github.com/",
-          logo_path: "github_logo.png",
-          duration: "July 2019 - PRESENT",
-          location: "Work From Home",
-          description:
-            "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
-          color: "#181717",
-        },
+      
+      
+     
+       
       ],
     },
   ],
@@ -540,7 +481,7 @@ const experience = {
 const projectsHeader = {
   title: "Projects",
   description:
-    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+    "Welcome to my projects showcase! Here, you'll find a collection of web applications and projects that demonstrate my skills as a MERN full-stack developer. From dynamic front-end interfaces to robust back-end services and seamless database integration, these projects highlight my ability to build and maintain full-scale web applications. Each project showcases different aspects of my expertise, including user authentication, RESTful API development, real-time data handling, and responsive design. Take a look to see how I bring ideas to life through code!",
   avatar_image_path: "projects_image.svg",
 };
 
